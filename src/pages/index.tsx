c2ee1d143ad496d314414d08dc190656a32b6109
Luiz Fernando Vital <luizfvital@gmail.com>
2021-04-21 15:24:54 -0300

    Added json-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-import {Header} from '../components/Header'
 
-export default function Home() {
+interface HomeProps {
+  episodes: any
+}
+
+export default function Home({episodes}: HomeProps) {
   return (
-    <Header />
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8, // 8 horas
+  }
 }
